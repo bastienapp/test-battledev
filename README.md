e209138f1c725423cc70eafdca43388e85ce191c
bastienwcs <bastien@wildcodeschool.fr>
2018-11-07 22:16:34 +0100

    solution

--- README.md
+++ README.md
@@ -7,3 +7,23 @@
 2. Travailler dans la classe `IsoContest`
 
 3. Lancer les tests : clic-droit sur le dossier `test` : *Run 'All Tests'*
+
+## Sujet
+
+### Objectif
+
+Il y a fort longtemps, dans une galaxie lointaine, très lointaine… Vous habitez une planète désertique à deux soleils, et êtes le modeste exploitant d’une ferme hydroponique. Votre exploitation est représentée par une grille carrée sur laquelle sont disposés des évaporateurs d’humidité. Chaque évaporateur d’humidité irrigue et rend ainsi cultivables les 8 cases qui lui sont adjacentes (on ne peut rien cultiver sur une case contenant un évaporateur d’humidité).
+
+Le but de ce challenge est de déterminer le nombre de cases cultivables de votre exploitation.
+
+
+### Données
+
+#### Entrée
+
+* Ligne 1 : un entier N compris entre 3 et 50 représentant la taille de votre ferme (une grille carrée de dimension N x N)
+* Lignes 2 à N+1 : les lignes de la carte représentées par des chaînes de N caractères. Les caractères de la ligne sont soit X (un évaporateur) soit . (une case vide).
+
+#### Sortie
+
+Un entier représentant le nombre de cases cultivables de votre ferme.
--- src/com/isograd/exercise/IsoContest.java
+++ src/com/isograd/exercise/IsoContest.java
@@ -4,16 +4,35 @@
  * Use: System.err.println to ouput debugging information to STDERR.
  * ***/
 package com.isograd.exercise;
-import java.util.*;
+
+import java.util.Scanner;
 
 public class IsoContest {
-    public static void main( String[] argv ) throws Exception {
-        String  line;
+    public static void main(String[] argv) throws Exception {
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNextLine()) {
-            line = sc.nextLine();
-            /* Lisez les données et effectuez votre traitement */
+        int n = sc.nextInt();
+
+        char[][] field = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            String line = sc.next();
+            field[i] = line.toCharArray();
         }
-        /* Vous pouvez aussi effectuer votre traitement une fois que vous avez lu toutes les données.*/
+
+        int cultivable = 0;
+        for (int x = 0; x < n; x++) {
+            for (int y = 0; y < n; y++) {
+                if (field[x][y] == 'X') {
+                    for (int i = Math.max(x - 1, 0); i <= Math.min(x + 1, n - 1); i++) {
+                        for (int j = Math.max(y - 1, 0); j <= Math.min(y + 1, n - 1); j++) {
+                            if (field[i][j] == '.') {
+                                field[i][j] = '#';
+                                cultivable++;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.print(cultivable);
     }
 }
