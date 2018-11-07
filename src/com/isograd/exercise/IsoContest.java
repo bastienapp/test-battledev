55fc239733af38e4c708bafc6c980c927ccf4cfb
bastienwcs <bastien@wildcodeschool.fr>
2018-11-07 21:24:26 +0100

    solution

--- src/com/isograd/exercise/IsoContest.java
+++ src/com/isograd/exercise/IsoContest.java
@@ -4,16 +4,42 @@
  * Use: System.err.println to ouput debugging information to STDERR.
  * ***/
 package com.isograd.exercise;
-import java.util.*;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
 
 public class IsoContest {
-    public static void main( String[] argv ) throws Exception {
-        String  line;
+    public static void main(String[] argv) throws Exception {
+
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNextLine()) {
-            line = sc.nextLine();
-            /* Lisez les données et effectuez votre traitement */
+        int myScores[] = new int[5];
+        for (int i = 0; i < 5; i++) {
+            myScores[i] = sc.nextInt();
         }
-        /* Vous pouvez aussi effectuer votre traitement une fois que vous avez lu toutes les données.*/
+        int nbFriends = sc.nextInt();
+        int nbBesties = sc.nextInt();
+        TreeMap<Integer, Integer> scores = new TreeMap<>();
+        for (int i = 0; i < nbFriends; i++) {
+            int friendScores[] = new int[5];
+            for (int j = 0; j < 5; j++) {
+                friendScores[j] = sc.nextInt();
+            }
+
+            int distance = 0;
+            for (int j = 0; j < 5; j++) {
+                distance += Math.abs(myScores[j] - friendScores[j]);
+            }
+
+            int score = sc.nextInt();
+            scores.put(distance, score);
+        }
+        int total = 0;
+        for (int i = 0; i < nbBesties; i++) {
+            Map.Entry<Integer, Integer> score = scores.firstEntry();
+            total += score.getValue();
+            scores.remove(score.getKey());
+        }
+        System.out.print(total / nbBesties);
     }
 }
