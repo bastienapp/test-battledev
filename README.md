9db26ce085c65d594c522fc85157c77387d7df4c
bastienwcs <bastien@wildcodeschool.fr>
2019-03-26 22:04:30 +0100

    en cours

--- README.md
+++ README.md
@@ -7,3 +7,38 @@
 2. Travailler dans la classe `IsoContest`
 
 3. Lancer les tests : clic-droit sur le dossier `test` : *Run 'All Tests'*
+
+Énoncé
+
+Ce challenge est une variante de "Salle au trésor". Ici, votre personnage atterrit au milieu d'un couloir rempli de pièces d'or et de multiplicateurs de richesse. Ce couloir est représenté par une chaîne de caractères (en effet, il est unidimensionnel), dont les caractères peuvent être :
+- o : pièce d'or, augmente votre richesse de 1
+- * : multiplicateur, double votre richesse
+- X : position initiale de votre personnage (ce caractère apparaît exactement une fois)
+
+(Il n'y a pas de case vide.)
+
+Objectif
+
+Le couloir étant étroit, si une case contient un objet, et que vous souhaitez accéder à la partie du couloir qui se trouve derrière, vous êtes obligé de le ramasser (et donc d'activer son effet) si vous voulez libérer le passage. L'objectif est toujours de déterminer la séquence de mouvements qui vous permettra de maximiser votre butin.
+
+Données
+
+Entrée
+
+Ligne 1 : un entier N compris entre 1 et 100, représentant la taille du couloir (nombre de caractères de la chaîne).
+Ligne 2 : une chaîne de caractères de longueur N, composée de o, * et X comme indiqué plus haut.
+
+Sortie
+
+Une chaîne de caractères, indiquant la suite de o et * que votre personnage ramassera dans l'ordre s'il se déplace de façon à optimiser le butin final.
+
+Exemple
+
+7
+*o*X**o
+
+La sortie attendue est *o**o*, ce qui correspond à :
+- d'abord prendre le multiplicateur immédiatement à gauche de la position initiale ;
+- ensuite prendre la pièce d'or à gauche ;
+- ensuite partir vers la droite et ramasser les 3 objets successivement rencontrés jusqu'au bout du couloir ;
+- enfin revenir à l'extrémité gauche et prendre le dernier multiplicateur.
--- src/com/isograd/exercise/IsoContest.java
+++ src/com/isograd/exercise/IsoContest.java
@@ -8,12 +8,65 @@ import java.util.*;
 
 public class IsoContest {
     public static void main( String[] argv ) throws Exception {
-        String  line;
+        String n;
         Scanner sc = new Scanner(System.in);
-        while(sc.hasNextLine()) {
-            line = sc.nextLine();
-            /* Lisez les données et effectuez votre traitement */
+        n = sc.nextLine();
+        String line = sc.nextLine();
+
+        doStuff(line, "", 0);
+    }
+
+    public static String doStuff(String line, String current, int score) {
+        if ((!line.contains("o") && !line.contains("*"))) {
+            return current;
         }
-        /* Vous pouvez aussi effectuer votre traitement une fois que vous avez lu toutes les données.*/
+
+        int s = 0;
+        int closest = line.length();
+        int pos = line.indexOf("X");
+        char search = line.contains("o") ? 'o' : '*';
+        StringBuilder newLine = new StringBuilder(line);
+        newLine.setCharAt(pos, '.');
+        line = newLine.toString();
+        while (line.substring(s).indexOf(search) != -1) {
+            int n = line.substring(s).indexOf(search);
+            if (Math.abs(pos - n) < closest) {
+                closest = n;
+            }
+            s = n;
+        }
+        String result = "";
+        if (closest < pos) {
+            // go left
+            current += "<";
+            int newPos = pos - 1;
+            int newScore = score;
+            if (line.charAt(newPos) == 'o') {
+                newScore++;
+                current += "x";
+            } else if (line.charAt(newPos) == '*') {
+                newScore *= 2;
+                current += "x";
+            }
+            newLine = new StringBuilder(line);
+            newLine.setCharAt(newPos, 'X');
+            result = doStuff(newLine.toString(), current, newScore);
+        } else {
+            // go right
+            current += ">";
+            int newPos = pos + 1;
+            int newScore = score;
+            if (line.charAt(newPos) == 'o') {
+                newScore++;
+                current += "x";
+            } else if (line.charAt(newPos) == '*') {
+                newScore *= 2;
+                current += "x";
+            }
+            newLine = new StringBuilder(line);
+            newLine.setCharAt(newPos, 'X');
+            result = doStuff(newLine.toString(), current, newScore);
+        }
+        return result;
     }
 }
